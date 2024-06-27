2148d830f1ccdab5e44515a4fa179f95ac0bb31f
Anna Yakimenko <qyri@ya.ru>
2024-06-27 15:49:09 +0300

    first

---
+++ src/main/java/qaguru/firstTest.java
@@ -0,0 +1,24 @@
+package qaguru;
+
+
+import org.junit.Test;
+
+@Test
+public class firstTest {
+    void firstTest {
+        //<input required="" autocomplete="off" placeholder="First Name" type="text" id="firstName" class=" mr-sm-2 form-control">
+    //   примеры xpath  и сss
+
+ $("[id=firstName]").setValue ("Ivanov");
+ $(by())
+
+
+
+
+
+    }
+
+
+
+
+}
